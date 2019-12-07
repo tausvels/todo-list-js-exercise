66dda6fabbe959d7eb511e2bdfae1ea5b5567de7
tausvels <engr.tausifkhan@gmail.com>
2019-12-06 19:49:00 -0800

    Modified index file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
+const newTask = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+function newTask(title, description) {
   taskTitles.push(title);
+  newTask.push(description);
   taskComplete.push(false);
 }
 
